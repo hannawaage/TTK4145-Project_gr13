6b0d41fb4b650f3d950fc836a557a7a51a91ff27
stefalarse <60001631+stefalarse@users.noreply.github.com>
2020-03-11 08:31:52 +0100

    Update testmain.go

--- Project/main/testmain.go
+++ Project/main/testmain.go
@@ -4,6 +4,9 @@ import (
 	. "../Orders"
 	. "../config"
 	. "../driver-go/elevio"
+//	"time"
+//	"fmt"
+
 )
 
 func main() {
@@ -12,42 +15,24 @@ func main() {
 
 	elevator := Elevator{
 		Id:     1,
-		Floor:  0,
-		Dir:    MD_Stop,
-		State:  Idle,
 		Orders: [NumFloors][NumButtons]bool{},
 	}
 
 	esmChns := EsmChns{
 		NewOrder: make(chan ButtonEvent),
-	}
-
-	signalChns := SignalChns{
 		Buttons: make(chan ButtonEvent),
 		Floors:  make(chan int),
 	}
-
 	Init("localhost:15657", numFloors)
 
-	var d MotorDirection = MD_Up
-	SetMotorDirection(d)
-
-	/*drv_buttons := make(chan ButtonEvent)
-	drv_floors := make(chan int)
-	drv_obstr := make(chan bool)
-	drv_stop := make(chan bool)*/
-
-	go PollButtons(signalChns.Buttons)
-	go PollFloorSensor(signalChns.Floors)
-	go GetLocalOrders(elevator, esmChns, signalChns)
-	go ShouldStop(elevator)
-	go SetDirection(elevator)
-	//go PollObstructionSwitch(drv_obstr)
-	//go PollStopButton(drv_stop)
-
+	go PollButtons(esmChns.Buttons)
+	go PollFloorSensor(esmChns.Floors)
+	go UpdateState(elevator, esmChns)
+	//go GetLocalOrders(elevator, esmChns, esmChns.NewOrder)
+//go SetDirection(elevator)
 	for {
 
-		//fmt.Printf("%+v\n", elevator.Orders)
+	//	fmt.Printf("%+v\n", elevator.Orders)
 
 		//time.Sleep(4000 * time.Millisecond)
 
