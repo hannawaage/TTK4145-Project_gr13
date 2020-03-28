df354256b97b446aeaaab391516791942b971800
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-03-28 11:48:29 +0100

    Test

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -190,8 +190,8 @@ func mergeAllOrders(id int, all [config.NumElevs][config.NumFloors][config.NumBu
 			continue
 		}
 		for floor := 0; floor < config.NumFloors; floor++ {
-			for btn := 0; btn < config.NumButtons-1; btn++ {
-				if all[elev][floor][btn] {
+			for btn := 0; btn < config.NumButtons; btn++ {
+				if all[elev][floor][btn] && (btn != config.NumButtons-1) {
 					merged[id][floor][btn] = true
 					all[elev][floor][btn] = false
 				}
