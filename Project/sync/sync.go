7975c55c23ae82345710665192c6a2e140465cf9
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-03-28 10:34:41 +0100

    Integrated Costfunction

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -47,20 +47,21 @@ func Sync(id string, syncCh config.SyncChns, esmChns config.EsmChns) {
 		}
 	}()
 
-	go func() {
-		for {
-			if currentAllOrders != updatedLocalOrders {
-				if !online {
-					updatedLocalOrders = mergeAllOrders(idDig, updatedLocalOrders)
-					esmChns.CurrentAllOrders <- updatedLocalOrders
-				} else {
-					esmChns.CurrentAllOrders <- currentAllOrders
+	/*
+		go func() {
+			for {
+				if currentAllOrders != updatedLocalOrders {
+					if !online {
+						updatedLocalOrders = mergeAllOrders(idDig, updatedLocalOrders)
+						esmChns.CurrentAllOrders <- updatedLocalOrders
+					} else {
+						esmChns.CurrentAllOrders <- currentAllOrders
+					}
+					currentAllOrders = updatedLocalOrders
 				}
-				currentAllOrders = updatedLocalOrders
+				time.Sleep(100 * time.Millisecond)
 			}
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
+		}()*/
 
 	localIP, err := localip.LocalIP()
 	if err != nil {
