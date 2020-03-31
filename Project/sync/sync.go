605659152b4240cf58b41249a3a6be45d3c66b5b
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-03-31 14:04:35 +0200

    Test med updateElev

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -40,6 +40,7 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
 					updatedLocalOrders[id] = elev.Orders
 				}
 				allElevs[id] = elev
+				updateElev = false
 			}
 		}
 	}()
@@ -103,7 +104,7 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
 					allElevs[recID].Orders = incomming.AllOrders[recID]
 					if masterID == id {
 						updatedLocalOrders = CostFunction(allElevs)
-						//updateElev = true
+						updateElev = true
 					} else if masterID == recID {
 						if currentAllOrders[id] == elev.Orders {
 							// Ikke noe nytt lokalt - ta inn det vi får fra master
