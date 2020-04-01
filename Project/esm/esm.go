4fcf7ebe84c7b5565eb9d3345f658b167f85aa2f
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-01 15:22:23 +0200

    Trying without deleting local order

--- Project/esm/esm.go
+++ Project/esm/esm.go
@@ -38,7 +38,7 @@ func RunElevator(esmChns config.EsmChns, id int) {
 			if elevator.Orders[newButtonOrder.Floor][newButtonOrder.Button] == false { //Hvis ikke allerede en ordre
 				elevator.Orders[newButtonOrder.Floor][newButtonOrder.Button] = true
 				go ShareElev(elevator, esmChns)
-				elevator.Orders[newButtonOrder.Floor][newButtonOrder.Button] = false //Så ordren ikke påvirker esm før kostfunksjonen har evaluert den
+				//elevator.Orders[newButtonOrder.Floor][newButtonOrder.Button] = false //Så ordren ikke påvirker esm før kostfunksjonen har evaluert den
 			}
 
 		case currentAllOrders := <-esmChns.CurrentAllOrders:
--- Project/esm/esmFunc.go
+++ Project/esm/esmFunc.go
@@ -28,6 +28,7 @@ func SetCurrentOrders(id int, elevator config.Elevator, currentAllOrders [config
 	for elev := 0; elev < config.NumElevs; elev++ {
 		for floor := 0; floor < config.NumFloors; floor++ {
 			for btn = 0; btn < config.NumButtons; btn++ {
+
 				if !currentAllOrders[elev][floor][btn] && elevator.Lights[elev][floor][btn] {
 					elevator.Lights[elev][floor][btn] = false
 					if !elevator.Orders[floor][btn] {
