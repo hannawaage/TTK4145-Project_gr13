0a34d70aee43674f74aa01f61309c7657d8072c4
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-18 18:13:12 +0200

    Trying to fix setcurrent

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -79,18 +79,6 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
 						}
 					}
 				}
-				allElevs[recID] = incomming.Elev
-				if id == masterID {
-					updatedAllOrders = CostFunction(id, allElevs, onlineIDs)
-				} else if recID == masterID {
-					updatedAllOrders = incomming.AllOrders
-				}
-				//esmChns.CurrentAllOrders <- updatedAllOrders
-				//currentAllOrders = updatedAllOrders
-				if currentAllOrders != updatedAllOrders {
-					esmChns.CurrentAllOrders <- updatedAllOrders
-					currentAllOrders = updatedAllOrders
-				}
 				if incomming.IsReceipt {
 					if incomming.MsgId == currentMsgID {
 						if !contains(receivedReceipt, recID) {
@@ -103,6 +91,19 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
 						}
 					}
 				} else {
+					allElevs[recID] = incomming.Elev
+					if id == masterID {
+						updatedAllOrders = CostFunction(id, allElevs, onlineIDs)
+					} else if recID == masterID {
+						updatedAllOrders = incomming.AllOrders
+					}
+					esmChns.CurrentAllOrders <- updatedAllOrders
+					currentAllOrders = updatedAllOrders
+					/*
+						if currentAllOrders != updatedAllOrders {
+							esmChns.CurrentAllOrders <- updatedAllOrders
+							currentAllOrders = updatedAllOrders
+						}*/
 					msg := config.Message{elev, updatedAllOrders, incomming.MsgId, true, localIP, id}
 					for i := 0; i < 5; i++ {
 						syncCh.SendChn <- msg
