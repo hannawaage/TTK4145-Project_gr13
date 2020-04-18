1dfa20922d7f3f1a33af4d1e9035e8079e396d76
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-18 18:30:31 +0200

    Trying to fix setcurrent

--- Project/esm/esmFunc.go
+++ Project/esm/esmFunc.go
@@ -33,7 +33,7 @@ func SetCurrentOrders(id int, elevator config.Elevator, currentAllOrders [config
 					if elevator.Lights[elev][floor][btn] != currentAllOrders[id][floor][btn] {
 						elevio.SetButtonLamp(btn, floor, currentAllOrders[id][floor][btn])
 						if !currentAllOrders[id][floor][btn] {
-							fmt.Println("Slår av lys pga current")
+							fmt.Println("Current lys", currentAllOrders[id][floor][btn])
 						}
 					}
 					elevator.Orders[floor][btn] = currentAllOrders[id][floor][btn]
