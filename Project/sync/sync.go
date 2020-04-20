ff3c909d06c0e1040b1401798c9e4c643b207b97
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-20 10:39:23 +0200

    Added timestamps

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -127,8 +127,8 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
                 esmChns.CurrentAllOrders <- updatedAllOrders
 				currentAllOrders = updatedAllOrders
 				fmt.Println("Order  timeout")
-				time.Sleep(10 * time.Second)
-                orderTimeStamps = [config.NumFloors]int{}
+				//time.Sleep(10 * time.Second)
+                //orderTimeStamps = [config.NumFloors]int{}
             }
 		}
 	}
