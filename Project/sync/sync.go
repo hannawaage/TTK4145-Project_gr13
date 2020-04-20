ed20606b176ddbea3926020fa2be48561b35e58d
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-20 10:55:58 +0200

    Added timestamps

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -122,6 +122,7 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
             if timeout {
 				if numOrderTimeouts == 0 {
 					faultyElev = FindFaultyElev(&currentAllOrders, &orderTimeStamps)
+					fmt.Println("Faulty: ", faultyElev)
 				}
 				updatedAllOrders = MergeAllOrders(id, updatedAllOrders)
 				if faultyElev >= 0 {
@@ -131,7 +132,7 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
                 esmChns.CurrentAllOrders <- updatedAllOrders
 				currentAllOrders = updatedAllOrders
 				fmt.Println("Order  timeout")
-				orderTimeStamps = [config.NumFloors]int{}
+				//orderTimeStamps = [config.NumFloors]int{}
 				//time.Sleep(10 * time.Second)
                 numOrderTimeouts++
             }
