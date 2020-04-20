f3f637990851a27d93c8a983fbcf6d6cbb29e3f4
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-20 14:15:48 +0200

    Fixed faulty

--- Project/driver-go/elevio/elevator_io.go
+++ Project/driver-go/elevio/elevator_io.go
@@ -38,7 +38,6 @@ type ButtonEvent struct {
 func Init(addr string, numFloors int) {
 	if _initialized {
 		fmt.Println("Driver already initialized!")
-		panic()
 		return
 	}
 	_numFloors = numFloors
