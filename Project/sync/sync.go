a0454c0a2453b2a7d7928f770697a7da3f3704db
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-04-20 13:13:28 +0200

    Timestamps working good, removed faultyelev from cost

--- Project/sync/sync.go
+++ Project/sync/sync.go
@@ -47,7 +47,7 @@ func Sync(id int, syncCh config.SyncChns, esmChns config.EsmChns) {
 	go func() {
 		for {
 			if id != faultyElev{
-				UpdateTimeStamp(&orderTimeStamps, &currentAllOrders, &allElevs, faultyElev)
+				UpdateTimeStamp(&orderTimeStamps, &currentAllOrders, &allElevs)
 				if TimeStampTimeout(&orderTimeStamps) {
 					go func() { syncCh.OrderTimeout <- true }()
 				}
